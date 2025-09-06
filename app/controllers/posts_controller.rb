644b36ccf4b72ea5d2bdc8b53a4d28702f1d658a
Pasek108 <pasartur108@gmail.com>
2025-09-06 18:48:41 +0200

    minor fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.user_id = session[:user_id]
+    @post.user_id = current_user.id
 
     if @post.save
       redirect_to root_path, notice: "Post created successfully."
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: "users/registrations" }
-
   root "posts#index"
+
+  devise_for :users, controllers: { registrations: "users/registrations" }
 
   resources :posts, only: [ :index, :new, :create ]
 
